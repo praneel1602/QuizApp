d329d047c5f7e7eb843b678a38dd48406e700390
praneel1602 <praneeluj@gmail.com>
2024-12-12 23:42:03 +0530

    Add files via upload

---
+++ Quiz.jsx
@@ -0,0 +1,146 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './Quiz.css';
+import { data } from '../../assets/data';
+
+const shuffleArray = (array) => {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+};
+
+const Quiz = () => {
+    const [shuffledData, setShuffledData] = useState([]);
+    const [index, setIndex] = useState(0);
+    const [lock, setLock] = useState(false);
+    const [score, setScore] = useState(0);
+    const [result, setResult] = useState(false);
+    const [time, setTime] = useState(10);  
+    const [quizStarted, setQuizStarted] = useState(false); 
+
+    const opt1 = useRef(null);
+    const opt2 = useRef(null);
+    const opt3 = useRef(null);
+    const opt4 = useRef(null);
+    const optarray = [opt1, opt2, opt3, opt4];
+
+    useEffect(() => {
+        const shuffled = shuffleArray(data);
+        setShuffledData(shuffled);
+    }, []);
+
+    useEffect(() => {
+        if (quizStarted && time > 0) {
+            const timer = setInterval(() => {
+                setTime((prevTime) => {
+                    if (prevTime === 1) {
+                        clearInterval(timer);
+                        setResult(true);
+                        // reset();
+                        return 0;
+                    }
+                    return prevTime - 1;
+                });
+            }, 1000);
+
+            return () => clearInterval(timer);
+        }
+    }, [quizStarted, time]);
+
+    const startQuiz = () => {
+        console.log("Quiz Started!"); 
+        setQuizStarted(true);  
+    };
+
+    const checkAns = (e, ans) => {
+        if (!lock) {
+            if (ans === shuffledData[index].ans) {
+                console.log('right');
+                e.target.classList.add('correct');
+                setScore((prevScore) => prevScore + 1);
+            } else {
+                console.log('wrong');
+                e.target.classList.add('incorrect');
+                optarray[shuffledData[index].ans - 1].current.classList.add('correct');
+            }
+            setLock(true);
+        }
+    };
+
+    const next = () => {
+        if (lock) {
+            if (index < shuffledData.length - 1) {
+                setIndex((prevIndex) => prevIndex + 1);
+                setLock(false);
+                optarray.forEach((opt) => opt.current.classList.remove('incorrect', 'correct'));
+            } else {
+                setResult(true);
+            }
+        }
+    };
+
+    const reset = () => {
+        setIndex(0);
+        setScore(0);
+        setShuffledData(shuffleArray(data));
+        setLock(false);
+        setResult(false);
+        setTime(10); 
+        setQuizStarted(false); 
+        optarray.forEach((opt) => opt.current.classList.remove('incorrect', 'correct'));
+    };
+
+    return (
+        <div className="container">
+            {result? (
+                <div>
+                    <h1>Yay! You completed the Quiz</h1>
+                    <p>Your score is {score}</p>
+                    <button className="reset" onClick={reset}>Reset</button>
+                </div>
+            ) : shuffledData.length > 0 ? (
+                <>
+                    {!quizStarted ? (
+                        <button onClick={startQuiz}>Start Quiz</button>
+                    ) : (
+                        <>
+                            <h1>Quiz</h1>
+                            <div className="timer">
+                                Time Left: {time}s
+                            </div>
+                            <h2>
+                                {index + 1}. {shuffledData[index].question}
+                            </h2>
+                            <hr />
+                            <ul>
+                                <li ref={opt1} onClick={(e) => checkAns(e, 1)}>
+                                    {shuffledData[index].option1}
+                                </li>
+                                <li ref={opt2} onClick={(e) => checkAns(e, 2)}>
+                                    {shuffledData[index].option2}
+                                </li>
+                                <li ref={opt3} onClick={(e) => checkAns(e, 3)}>
+                                    {shuffledData[index].option3}
+                                </li>
+                                <li ref={opt4} onClick={(e) => checkAns(e, 4)}>
+                                    {shuffledData[index].option4}
+                                </li>
+                            </ul>
+                            <button onClick={next}>Next</button>
+                            <div className="number">
+                                {index + 1} out of {shuffledData.length} questions
+                            </div>
+                            
+                        </>
+                    )}
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default Quiz;
---
+++ data.js
@@ -0,0 +1,82 @@
+export const  data=[
+    {
+        question:"Which is the largest continent in the world?",
+        option1:"Asia",
+        option2:"Africa",
+        option3:"Australia",
+        option4:"Europe",
+        ans:1
+    },
+    {
+        question:"Which is the smallest continent in the world?",
+        option1:"Asia",
+        option2:"Africa",
+        option3:"Australia",
+        option4:"Europe",
+        ans:3
+    },
+    {
+        question:"Which is the largest country in the world?",
+        option1:"Azer baijan",
+        option2:"South Africa",
+        option3:"Australia",
+        option4:"Russia",
+        ans:3
+    },
+    {
+        question:"Which is the largest Animal in the world?",
+        option1:"Donkey",
+        option2:"Anaconda",
+        option3:"Blue Whale",
+        option4:"Dolphin",
+        ans:3
+    },
+    {
+        question:"Which is the smallest continent in the world?",
+        option1:"Asia",
+        option2:"Africa",
+        option3:"Australia",
+        option4:"Europe",
+        ans:3
+    },
+    {
+        question:"Which is the contineent and country among the following ?",
+        option1:"Asia",
+        option2:"Africa",
+        option3:"Australia",
+        option4:"Europe",
+        ans:3
+    },
+    {
+        question:"Which is the largest bird in the world?",
+        option1:"Ostrich",
+        option2:"Emu",
+        option3:"Crow",
+        option4:"Eagle",
+        ans:1
+    },
+    {
+        question:"Which is a flightless bird?",
+        option1:"sparrow",
+        option2:"piegon",
+        option3:"crow",
+        option4:"penguin",
+        ans:4
+    },
+    {
+        question:"Which is the fastest animal in the world?",
+        option1:"Horse",
+        option2:"Cheetah",
+        option3:"Anaconda",
+        option4:"Emu",
+        ans:2
+    },
+    {
+        question:"Which is the flower from the follwoing?",
+        option1:"Asia",
+        option2:"Africa",
+        option3:"Australia",
+        option4:"jasmine",
+        ans:4
+    } 
+];
